70ff74f07955a48f993744bf9f9389c6d350bea7
Priyanka Kumari <kumaripriyanka1832@gmail.com>
2024-06-12 10:06:32 +0530

    completed

--- index.js
+++ index.js
@@ -1,128 +1,204 @@
 const todoButton = document.querySelector('.todo-btn');
 const todoInput = document.querySelector('.todo-input');
 const todoList = document.querySelector('.todo-list');
+const completedFilterBtn = document.querySelector('.completed-filter');
+const allFilterBtn = document.querySelector('.all-filter');
+const remainingFilterBtn = document.querySelector('.remaining-filter');
+
+const getTodoListFromLocalStorage = () => {
+  let todoListArr;
+  if (localStorage.getItem('todoList')) {
+    todoListArr = JSON.parse(localStorage.getItem('todoList'));
+  } else {
+    todoListArr = [];
+  }
+  return todoListArr;
+};
 
 const init = () => {
-    let todoListArr;
-    if(localStorage.getItem('todoList')){
-        todoListArr = JSON.parse(localStorage.getItem('todoList'));
-    }
-    else{
-        todoListArr = [];
+  let todoListArr = getTodoListFromLocalStorage();
+  todoList.innerHTML = ``;
+  todoListArr.forEach((todo) => {
+    const newTodo = document.createElement('div');
+    newTodo.classList.add('todo');
 
-    }
-   
-    todoListArr.forEach((todo) => {
-        const  newTodo  = document.createElement('div');
-        newTodo.classList.add('todo');
+    const newTodoListItem = document.createElement('li');
+    newTodoListItem.innerText = todo.text;
+    if (todo.completed) newTodoListItem.classList.add('mark-complete');
 
-        const newTodoListItem = document.createElement('li');
-        newTodoListItem.innerText = todo.Text;
-        if(todo.completed) newTodoListItem.classList.add('mark-complete');
+    const newTodoBtnContainer = document.createElement('div');
+    newTodoBtnContainer.classList.add('todo-btn-container');
 
-        const newTodoBtnContainer = document.createElement('div');
-        newTodoBtnContainer.classList.add('todo-btn-container');
+    const completeBtn = document.createElement('button');
+    completeBtn.classList.add('complete-btn');
 
-        const completeBtn =  document.createElement('button');
-        completeBtn.classList.add('complete-btn');
+    const deleteBtn = document.createElement('button');
+    deleteBtn.classList.add('trash-btn');
 
-        const deleteBtn = document.createElement('button');
-        deleteBtn.classList.add('trash-btn');
-     
-        completeBtn.innerHTML =  '<i class="fa fa-check"></i>';
-        deleteBtn.innerHTML = ' <i class="fa fa-trash"></i>';
+    completeBtn.innerHTML = `<i class="fa fa-check"></i>`;
+    deleteBtn.innerHTML = `<i class="fa fa-trash"></i>`;
 
-        newTodoBtnContainer.appendChild(completeBtn);
-        newTodoBtnContainer.appendChild(deleteBtn);
-     
-        newTodo.appendChild(newTodoListItem);
-        newTodo.appendChild(newTodoBtnContainer);
+    newTodoBtnContainer.appendChild(completeBtn);
+    newTodoBtnContainer.appendChild(deleteBtn);
 
-        todoList.appendChild(newTodo);
-       
-    });
+    newTodo.appendChild(newTodoListItem);
+    newTodo.appendChild(newTodoBtnContainer);
+
+    todoList.appendChild(newTodo);
+  });
 };
 
-document.addEventListener('DOMContentLoaded',init);
+document.addEventListener('DOMContentLoaded', init);
 
-todoButton.addEventListener('click',(event) => {
+todoButton.addEventListener('click', (event) => {
+  event.preventDefault();
+  const todoText = todoInput.value;
+  if (!todoText) return;
+  saveTodoListToLocalStorage(todoText);
 
-    event.preventDefault();
-    //extracting todo value
-    const todoText = todoInput.value;
-    //check for empty list
-    if(!todoText) return;
-    //save to local storage
-    saveTodoListToLocalStorage(todoText);
+  const newTodo = document.createElement('div');
+  newTodo.classList.add('todo');
 
-   const  newTodo  = document.createElement('div');
-   newTodo.classList.add('todo');
-   const newTodoListItem = document.createElement('li');
-   newTodoListItem.innerText = todoText;
+  const newTodoListItem = document.createElement('li');
+  newTodoListItem.innerText = todoText;
 
-   const newTodoBtnContainer = document.createElement('div');
-   newTodoBtnContainer.classList.add('todo-btn-container');
+  const newTodoBtnContainer = document.createElement('div');
+  newTodoBtnContainer.classList.add('todo-btn-container');
 
-   const completeBtn =  document.createElement('button');
-   completeBtn.classList.add('complete-btn');
+  const completeBtn = document.createElement('button');
+  completeBtn.classList.add('complete-btn');
 
-   const deleteBtn = document.createElement('button');
-   deleteBtn.classList.add('trash-btn');
+  const deleteBtn = document.createElement('button');
+  deleteBtn.classList.add('trash-btn');
 
-   completeBtn.innerHTML =  '<i class="fa fa-check"></i>';
-   deleteBtn.innerHTML = ' <i class="fa fa-trash"></i>';
-   newTodoBtnContainer.appendChild(completeBtn);
-   newTodoBtnContainer.appendChild(deleteBtn);
+  completeBtn.innerHTML = `<i class="fa fa-check"></i>`;
+  deleteBtn.innerHTML = `<i class="fa fa-trash"></i>`;
 
-   newTodo.appendChild(newTodoListItem);
-   newTodo.appendChild(newTodoBtnContainer);
+  newTodoBtnContainer.appendChild(completeBtn);
+  newTodoBtnContainer.appendChild(deleteBtn);
 
-   todoList.appendChild(newTodo);
-   todoInput.value = '';
-    
+  newTodo.appendChild(newTodoListItem);
+  newTodo.appendChild(newTodoBtnContainer);
 
+  todoList.appendChild(newTodo);
+  todoInput.value = '';
 });
+
+const updateList = (todoListArr) => {
+  todoList.innerHTML = ``;
+  todoListArr.forEach((todo) => {
+    const newTodo = document.createElement('div');
+    newTodo.classList.add('todo');
+
+    const newTodoListItem = document.createElement('li');
+    newTodoListItem.innerText = todo.text;
+    if (todo.completed) newTodoListItem.classList.add('mark-complete');
+
+    const newTodoBtnContainer = document.createElement('div');
+    newTodoBtnContainer.classList.add('todo-btn-container');
+
+    const completeBtn = document.createElement('button');
+    completeBtn.classList.add('complete-btn');
+
+    const deleteBtn = document.createElement('button');
+    deleteBtn.classList.add('trash-btn');
+
+    completeBtn.innerHTML = `<i class="fa fa-check"></i>`;
+    deleteBtn.innerHTML = `<i class="fa fa-trash"></i>`;
+
+    newTodoBtnContainer.appendChild(completeBtn);
+    newTodoBtnContainer.appendChild(deleteBtn);
+
+    newTodo.appendChild(newTodoListItem);
+    newTodo.appendChild(newTodoBtnContainer);
+
+    todoList.appendChild(newTodo);
+  });
+};
 
 const saveTodoListToLocalStorage = (todo) => {
-   let todoListArr;
-
-   if(localStorage.getItem('todoList')){
-    todoListArr  = JSON.parse(localStorage.getItem('todoList'));
-   }
-   else{
-    todoListArr = [];
-   }
-   
-   //console.log(todoListArr);
-   todoListArr.push({ text: todo, completed:false });
-
-   localStorage.setItem('todoList',JSON.stringify(todoListArr));
-    
+  let todoListArr = getTodoListFromLocalStorage();
+  todoListArr.push({ text: todo, completed: false });
+  localStorage.setItem('todoList', JSON.stringify(todoListArr));
 };
 
-
-todoList.addEventListener('click',(e) => {
-    const item  = e.target;
-    if(
-        item.classList.contains('trash-btn')||
-        item.classList.contains('fa-trash')
-    ) {
-        const elementType = item.classList.contains('fa-trash') ? 1:2;
-     const btnContainer = 
-     elementType == 1 ? item.parentElement.parentElement : item.parentElement;
-     const todoListItem = btnContainer.previousElementSibling;
-     const todoText = todoListItem.textContent.trim();
-     console.log(todoText);
-    }
+todoList.addEventListener('click', (e) => {
+  const item = e.target;
+  if (item.classList.contains('trash-btn') || item.classList.contains('fa-trash')) {
+    const elementType = item.classList.contains('fa-trash') ? 1 : 2;
+    const btnContainer = elementType === 1 ? item.parentElement.parentElement : item.parentElement;
+    const todoListItem = btnContainer.previousElementSibling;
+    const todoText = todoListItem.textContent.trim();
+    deleteTodo(todoText);
+  } else if (item.classList.contains('complete-btn') || item.classList.contains('fa-check')) {
+    const elementType = item.classList.contains('fa-check') ? 1 : 2;
+    const btnContainer = elementType === 1 ? item.parentElement.parentElement : item.parentElement;
+    const todoListItem = btnContainer.previousElementSibling;
+    const todoText = todoListItem.textContent.trim();
+    markComplete(todoText);
+  }
 });
 
+const markComplete = (todoText) => {
+  let todoListArr = getTodoListFromLocalStorage();
+  const updatedTodoList = todoListArr.map((todo) => {
+    if (todo.text === todoText) {
+      return { text: todo.text, completed: !todo.completed };
+    }
+    return todo;
+  });
+  localStorage.setItem('todoList', JSON.stringify(updatedTodoList));
+  init();
+};
 
-// const list = ['jsdjdj','kdfldf','lkdjfldjf'];
-// console.log(JSON.stringify(list));
+const deleteTodo = (todoText) => {
+  let todoListArr = getTodoListFromLocalStorage();
+  const updatedTodoList = todoListArr.filter((todo) => {
+    return todo.text !== todoText;
+  });
+  localStorage.setItem('todoList', JSON.stringify(updatedTodoList));
+  init();
+};
 
-// const obj = {
-//     name: 'Aditya',
-//     age: 23,
-//     lastName: 'Agrawal', 
-// };
-// console.log(JSON.stringify(obj));
+completedFilterBtn.addEventListener('click', (e) => {
+  let todoListArr = getTodoListFromLocalStorage();
+  const completedTodoList = todoListArr.filter((todo) => {
+    return todo.completed === true;
+  });
+  updateList(completedTodoList);
+});
+
+allFilterBtn.addEventListener('click', (e) => {
+  let todoListArr = getTodoListFromLocalStorage();
+  updateList(todoListArr);
+});
+
+remainingFilterBtn.addEventListener('click', (e) => {
+  let todoListArr = getTodoListFromLocalStorage();
+  const remainingTodoList = todoListArr.filter((todo) => {
+    return todo.completed === false;
+  });
+  updateList(remainingTodoList);
+});
+// // Add event listners for all-filter and remaining-filter
+
+// /**
+//  * 1. Select
+//  * 2. Add Event Listners for Each of Them
+//  */
+
+// // JSON Format -> JSON.stringify, JSON.parse
+// // const list = ['ljsndg', 'jsg', 'ksnbgs'];
+// // console.log(JSON.stringify(list));
+
+// // const obj = {
+// //   name: 'Aditya',
+// //   age: 23,
+// //   lastName: 'Agrawal',
+// //   address: {
+// //     city: 'Bangalore',
+// //     state: 'Karnataka',
+// //   },
+// // };
+// // console.log(obj);
+// // console.log(JSON.stringify(obj));
